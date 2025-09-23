d97274f0bc2ba324671118189736a94dbc75c563
Mohammad Hashemian <muhammadhashemian@gmail.com>
2025-09-23 10:31:10 +0330

    Removing deprecated model files

--- model_dep/patient.py
+++
@@ -1,7 +0,0 @@
-class Patient:
-    def __init__(self, initial_age, initial_joint_score):
-        self.alive = True
-        self.age = initial_age
-        self.jointScore = initial_joint_score
-        self.state = "alive_wo_arthropathy"
-        self.treatment = "prophylaxis"
