461539138dbaf13b2a14db7d0da589e85e43cc6b
Mohammad Hashemian <muhammadhashemian@gmail.com>
2026-07-18 20:45:14 +0330

    fix: ignoring noisy logs

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
 import logging
 
-logging.getLogger("pymc").setLevel(logging.WARNING)
+logging.getLogger("pymc.stats.convergence").setLevel(logging.CRITICAL)
+logging.getLogger("pytensor.configparser").setLevel(logging.WARNING)
